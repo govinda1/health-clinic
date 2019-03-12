a0fff0ddee9bf945a92ce0e2c3652b59c863a35f
Govinda Dhirde <>
2019-03-12 17:18:33 -0400

    Added model class - doctor

---
+++ src/main/java/com/sample/web/model/Doctor.java
@@ -0,0 +1,58 @@
+package com.sample.web.model;
+
+public class Doctor {
+	
+	private Integer doc_id;
+	private String doc_name;
+	private String doc_degree;
+	private String doc_speciality;
+	private String doc_city;
+	private String doc_state;
+	private Integer doc_ph;
+	public Integer getDoc_id() {
+		return doc_id;
+	}
+	public void setDoc_id(Integer doc_id) {
+		this.doc_id = doc_id;
+	}
+	public String getDoc_name() {
+		return doc_name;
+	}
+	public void setDoc_name(String doc_name) {
+		this.doc_name = doc_name;
+	}
+	public String getDoc_degree() {
+		return doc_degree;
+	}
+	public void setDoc_degree(String doc_degree) {
+		this.doc_degree = doc_degree;
+	}
+	public String getDoc_speciality() {
+		return doc_speciality;
+	}
+	public void setDoc_speciality(String doc_speciality) {
+		this.doc_speciality = doc_speciality;
+	}
+	public String getDoc_city() {
+		return doc_city;
+	}
+	public void setDoc_city(String doc_city) {
+		this.doc_city = doc_city;
+	}
+	public String getDoc_state() {
+		return doc_state;
+	}
+	public void setDoc_state(String doc_state) {
+		this.doc_state = doc_state;
+	}
+	public Integer getDoc_ph() {
+		return doc_ph;
+	}
+	public void setDoc_ph(Integer doc_ph) {
+		this.doc_ph = doc_ph;
+	}
+
+	
+	
+	
+}
